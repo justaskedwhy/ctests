c54585c38a6c49c02a7ff3bdcaf3682b84cdf796
justaskedwhy <98099506+justaskedwhy@users.noreply.github.com>
2023-09-06 15:46:19 +0530

    Add files via upload

--- hello.c
+++ hello.c
@@ -1,24 +1,19 @@
+//program writen at 3:37pm
 #include<stdio.h>
-#include<stdlib.h>
-#define frequency_size 30
-#define sample_size frequency_size*10
-int main(){
-    srand(3);
-    int s[sample_size] = {0};
-    int n[frequency_size] = {0};
-    for(int i = 0; i < sample_size;i++){
-        s[i] = rand()%frequency_size;
-    }
-    for(int i = 0; i < sample_size;i++){
-        n[s[i]]++;
-    }
-    printf("%s%10s\n","Number","Frequency");
-    for(int i = 0; i < frequency_size ; i++ ){
-        printf("%6d ",i);
-        for (int j = 0; j < n[i];j++){
-            printf("*");
-        }
-        printf("\n");
+int main(void){
+    int startf = 0;//start of the fahrenheit scale
+    int endf = 0;//end of the fahrenheit scale
+    printf("Start?");
+    scanf("%d",&startf);//taking value form user (start)
+    printf("End?");
+    scanf("%d",&endf);//taking value form user (end)
+    /*the next puts and printf statements are for the table*/
+    puts("+-------------------------------------------------+");
+    printf("|%14s    |%11s    |%10s    |\n","Fahrenheit","Celsius","Kelvin");
+    puts("+-------------------------------------------------+");
+    for(startf;startf <= endf;startf++){
+        double celsius = (double) (startf - 32)*5/9;
+        printf("|%14d    |%11.2f    |%10.2f    |\n",startf,celsius,celsius + 273.15);
+        puts("+-------------------------------------------------+");
     }
 }
-    
